ce2fd63774abe904b16bfc22add367bcacaca7b9
js _luv <aljwharah.aljmhoor@gmail.com>
2019-05-02 00:42:55 +0300

    Final project

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 
     var SavedCompany = [];
-    
+    $('#result').hide();
       $('form').on('submit', function(event){
+        
         event.preventDefault();
           var companyName = $('#company').val();
+          $('#result').show();
           $.ajax ({
               method: 'GET',
               url: 'https://autocomplete.clearbit.com/v1/companies/suggest?query='+ companyName ,
@@ -15,10 +17,11 @@ $(document).ready(function () {
                      $('#domain').text('Domain:   '+ data[0].domain);
                      $('#logo').attr('src' ,data[0].logo );
                      $('<br>').appendTo('#result')
-                     $('<button>').text('Save').attr('type','click').appendTo('#result').on('click', function(){
+                     $('<button>').text('Save').appendTo('#result').on('click', function(){
                          SavedCompany.push(data);
                          localStorage.setItem('SavedCompany', JSON.stringify(SavedCompany));
                          renderSavedCompany();
+
                          
                      });
                      
@@ -33,11 +36,13 @@ $(document).ready(function () {
     
  
     function renderSavedCompany (){
-       
+       $('div > button').hide();
+       $('#saved-company').empty();
+       $('<h3>').text('saved companies:').appendTo('#saved-company');
         SavedCompany.forEach(function(data){
-          
-                  $('<p>').text(data[0].name).appendTo('#saved-company');
-                  $('<p>').text('Domain:   '+ data[0].domain).appendTo('#saved-company');
+                   $('p').hide();
+                  $('<h1>').text(data[0].name).appendTo('#saved-company');
+                  $('<h3>').text('Domain:   '+ data[0].domain).appendTo('#saved-company');
                   $('<img>').attr('src' ,data[0].logo ).appendTo('#saved-company')
                 })
 
